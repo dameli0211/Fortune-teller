5b0c0e3940e2f4564f3a03394f1a23aeebfbfb30
dameli0211 <dameli0211@gmail.com>
2021-03-02 13:11:18 -0500

    Declered varables and concatenated them

---
+++ main.js
@@ -0,0 +1,5 @@
+var numOfChildren= 2; 
+var nameofSpose= "John";
+var nameOfCity= "Sunnyvale";
+var titleofJob= "Software engeneer";
+console.log("I have " + numOfChildren + " kids" + " and married to " + nameofSpose + ". " + "I live in " + nameOfCity + ". " + "I am " + titleofJob + ".");
